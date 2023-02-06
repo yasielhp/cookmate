95b8dc9197d8db81e430c36bc819248f372201e2
Yasiel Hernández Portal <yasielhp@gmail.com>
2023-02-06 22:54:41 +0000

    feat: :lipstick: UI Pages/Home and Page/CreateRecipe

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -1,5 +1,14 @@
 export const CreateRecipe = () => {
   return (
-    <div>CreateRecipe</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-Gray text-[32px]'>
+          Create
+        </h1>
+        <p className='mt-2 text-Gray text-opacity-70 max-w-[500px]'>
+          Enter the ingredients you have in your refrigerator to generate a recipe you can make
+        </p>
+      </div>
+    </section>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,14 @@
 export const Home = () => {
   return (
-    <div>Home</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-Gray text-[32px]'>
+          Community recipes
+        </h1>
+        <p className='mt-2 text-Gray text-opacity-70 max-w-[500px]'>
+          Explore a collection of recipes created by the community using cohere's artificial intelligence
+        </p>
+      </div>
+    </section>
   )
 }
